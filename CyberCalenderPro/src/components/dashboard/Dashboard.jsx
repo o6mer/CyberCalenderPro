db988e8fa91aae43bb6cd8528e2c82c5a00f1422
o6mer <o6merliraz@gmail.com>
2023-01-10 15:45:38 +0200

    get requests

--- CyberCalenderPro/src/components/dashboard/Dashboard.jsx
+++ CyberCalenderPro/src/components/dashboard/Dashboard.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import NewClassForm from "./NewClassForm";
 import RequestList from "./RequestList/RequestList";
 import TopNavBar from "./TopNavBar/TopNavBar";
 
 const Dashboard = () => {
+  useEffect(() => {
+    const getRequests = async () => {
+      try {
+        const res = await axios.get("http://localhost:2000/unresolved");
+        console.log(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getRequests();
+  }, []);
+
   return (
     <main className="w-full h-full flex flex-col bg-primary">
       <TopNavBar />

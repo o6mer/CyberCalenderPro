5ec916d54bf846d501d37bb0424997ad8f7362e6
o6mer <o6merliraz@gmail.com>
2023-01-10 10:31:00 +0200

    please help

--- CyberCalenderPro/src/components/dashboard/Dashboard.jsx
+++ CyberCalenderPro/src/components/dashboard/Dashboard.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import { TextField } from "@mui/material";
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <main className="w-full h-full flex justify-center items-center">
+      <form action="">
+        <TextField label="Class Name" variant="outlined" type="text" />
+        <TextField label="Capacity" variant="outlined" type="number" />
+        <FormControlLabel control={<Checkbox defaultChecked />} label="AC" />
+        <FormControlLabel control={<Checkbox defaultChecked />} label="Zoom" />
+        <FormControlLabel control={<Checkbox defaultChecked />} label="PC's" />
+      </form>
+    </main>
+  );
 };
 
 export default Dashboard;

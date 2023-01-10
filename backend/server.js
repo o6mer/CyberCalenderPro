7ae810e3b29b004fcfcfa9d81fe3e8ba821fb108
netpes <arbivnetanel@gmail.com>
2023-01-10 13:00:22 +0200

    nodemon

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 2000;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const classes = require("./routes/class");
-
+const users = require("./routes/user");
 
 app.set("views", __dirname + "/views");
 app.engine("html", require("ejs").renderFile);
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({ origin: " http://localhost:5173" }));
 app.use("/", classes);
-
+app.use("/", users);
 
 
 

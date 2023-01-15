3a10895c88942daca9481ec44e4aa437aa6cf898
netpes <arbivnetanel@gmail.com>
2023-01-15 11:00:04 +0200

    fix auth 2

--- CyberCalenderPro/src/components/main/Advanced/handles/createMeeting.jsx
+++ CyberCalenderPro/src/components/main/Advanced/handles/createMeeting.jsx
@@ -1,7 +1,9 @@
 export default function AddMeeting(theDate,Thetime_range,ThegroupSize, class_name){
+
     const date = theDate
     const time_range = Thetime_range;
     const groupSize = ThegroupSize;
     const className = class_name;
+
 
 }

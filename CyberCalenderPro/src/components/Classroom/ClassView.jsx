2b5fa9f3c7f8f30cae2c5cc91d1ed1270bbbc251
roei zaro <roeip21@gmail.com>
2023-01-15 18:47:53 +0200

    Removed crashing line (not important)

--- CyberCalenderPro/src/components/Classroom/ClassView.jsx
+++ CyberCalenderPro/src/components/Classroom/ClassView.jsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
-import { useParams } from 'react-router-dom';
-import { UserContext } from '../../contexts/UserContext';
-import { Box, Button, Typography } from '@mui/material';
-import { DataGrid } from '@mui/x-data-grid';
-import Modal from '@mui/material/Modal';
+
 
 export default function ClassView() {
-    const param = useParams
+    
     return (
         <>
         
